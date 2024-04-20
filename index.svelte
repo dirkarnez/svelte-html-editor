1d832f56d7b3a7b57b57ea047dc7472df5699079
Dirk Arnez <smalldirkalex@gmail.com>
2024-04-20 20:42:49 +0800

    - add index.svelte

---
+++ index.svelte
@@ -0,0 +1,14 @@
+<script>
+	// Declare a variable to store the markdown data
+  let htmlCode = `<h1>HTML< Editor</h1><script>alert("hi!")<\/script>`;
+</script>
+
+<textarea bind:value={htmlCode} placeholder="Enter html here"/>
+
+<iframe srcdoc={htmlCode} title="testing"></iframe>
+
+<style>
+	iframe {
+		width: 100%;
+	}
+</style>
